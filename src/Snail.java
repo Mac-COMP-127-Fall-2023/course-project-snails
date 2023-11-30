9eb2864ea3f7718568eca9e705c3cfae1da66ec3
t-o-b-i-e <144360934+t-o-b-i-e@users.noreply.github.com>
2023-11-30 10:39:03 -0600

    internal logic for snail movement

--- src/Snail.java
+++ src/Snail.java
@@ -1,3 +1,90 @@
 public class Snail {
+
+    // position of top left corner
+    private int x, y;
     
+    private boolean attached;
+
+    // the side that the snail is attached to
+    enum Orientation {
+        BOTTOM,
+        LEFT,
+        TOP,
+        RIGHT
+    }
+    Orientation snailBottomOrientation;
+
+    // current velocity while falling
+    private int velocity = 0;
+
+    public Snail(int x, int y) {
+        this.x = x;
+        this.y = y;
+        snailBottomOrientation = Orientation.BOTTOM;
+        attached = true;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean getAttached() {
+        return attached;
+    }
+
+    public Orientation getOrientation() {
+        return snailBottomOrientation;
+    }
+
+    public void moveRight() {
+        move(1);
+    }
+
+    public void moveLeft() {
+        move(-1);
+    }
+
+    /**
+     * Moves the snail according to its orientation when the right
+     * arrow key is pressed. Then checks for collisions (unimplemented)
+     */
+    private void move(int m) {
+        switch (snailBottomOrientation) {
+            case BOTTOM:
+                x+=m;
+                break;
+            case LEFT:
+                y+=m;
+                break;
+            case TOP:
+                x-=m;
+                break;
+            case RIGHT:
+                y-=m;
+                break;
+        }
+        SnailGame.checkCollisions();
+    }
+
+    /**
+     * Accelerates the snail downwards and then checks for collisions
+     */
+    private void fall() {
+        velocity+=2;
+        y+=velocity;
+        SnailGame.checkCollisions();
+    }
+
+    public void setAttached(boolean attached) {
+        this.attached = attached;
+    }
+
+    public void setOrientation(Orientation newOrientation) {
+        this.snailBottomOrientation = newOrientation;
+    }
+
 }
--- src/SnailGame.java
+++ src/SnailGame.java
@@ -21,4 +21,18 @@ public class SnailGame {
     public static void main(String[] args) {
         SnailGame game = new SnailGame();
     }
+
+    /**
+     * called after our snail moves or falls, should check collisions
+     * and update its orientation if necessary. **We also need to check
+     * if one pixel below the center of the snail bottom is no longer
+     * colliding with a tile, this means that it is most of the way up
+     * a convex portion of the level, so we should rotate it the rest
+     * of the way so it doesn't look weird.** 
+     * idk if im explaining that poorly lol and there's probably a 
+     * better way to do that
+     */
+    public static void checkCollisions() {
+
+    }
 }
