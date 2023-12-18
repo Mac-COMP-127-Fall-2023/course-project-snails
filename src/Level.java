3d82eed03c323d14ff6b261fa48673c9078cade9
olivepchaps <olivepchaps@gmail.com>
2023-12-17 18:43:05 -0600

    added more tiles

--- src/Level.java
+++ src/Level.java
@@ -21,7 +21,7 @@ class Level {
 
     public static final int SCREEN_PIXELS_PER_TILE = PIXELS_PER_TILE * SnailGame.SCREEN_PIXEL_RATIO;
 
-    private List<Character> blockKeys = List.of('￣', '＿', '「', '」', '・', '／', '４', '＋', '～','＝','｛','｝','⊕','⊛','✚','＊', 'あ');
+    private List<Character> blockKeys = List.of('￣', '＿', '「', '」', '・', '／', '４', '＋', '～','＝','｛','｝','⊕','⊛','✚','＊', 'あ', '上', '下', '左', '右','ー','｜','四');
     private List<Character> decorationKeys = List.of('フ','ブ','プ','ラ','ル','ロ');
     private List<Character> platformKeys = List.of('ヒ', 'ビ', 'ピ', 'ひ', 'び');
     private Character endpointKey = 'Ⓕ';
